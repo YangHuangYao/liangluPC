3f4609e9e9f5f495ded4180e7b01ca1bb835ea5c
YangHuangYao <841115691@qq.com>
2018-06-10 12:35:56 +0800

    lianglu

---
+++ src/page/product/detail.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="detail-container body-bg">
+      <div class="app-container-width">
+        <el-row :gutter="20">
+          <el-col :span="18" class="detail-left">
+            <el-row class="detail-header">
+              <el-col :span="8">
+                d
+              </el-col>
+              <el-col :span="16">
+                dd
+              </el-col>
+            </el-row>
+          </el-col>
+          <el-col :span="6" class="detail-right">
+            <div class="detail-right-ground">
+
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+      name:"Detail",
+      data(){
+          return{
+
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/routers/detail.js
@@ -0,0 +1,10 @@
+/***
+ * 商品详情
+ * ***/
+export default [
+  {
+    path: '/product/detail',
+    name:'Detail',
+    component: resolve => require(['../../page/product/detail'], resolve)
+  }
+]
